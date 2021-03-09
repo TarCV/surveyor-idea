667fc9a97196509d97d8fd9a00aedf37ede302e6
GitHub Action <action@github.com>
2021-03-09 02:17:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/tarcv/surveyoridea/MyBundle.kt
+++ src/main/kotlin/com/github/tarcv/surveyoridea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tarcv.surveyoridea
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tarcv/surveyoridea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tarcv/surveyoridea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tarcv.surveyoridea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tarcv.surveyoridea.MyBundle
 
 class MyApplicationService {
 
