65a3bbdd6b190c5e10e1007ab41b859f321e378c
TarCV <2857607+TarCV@users.noreply.github.com>
2021-07-29 03:41:53 +0300

    Improve highlighting/autocomplete implementation

--- plugin/src/main/kotlin/com/github/tarcv/surveyoridea/gui/LocateToolWindow.kt
+++ plugin/src/main/kotlin/com/github/tarcv/surveyoridea/gui/LocateToolWindow.kt
@@ -1,5 +1,7 @@
 package com.github.tarcv.surveyoridea.gui
 
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiSelector
 import com.github.tarcv.surveyoridea.filetypes.uix.Hierarchy
 import com.github.tarcv.surveyoridea.services.LocateToolHoldingService
 import com.github.tarcv.testingteam.surveyor.Evaluator
@@ -17,23 +19,29 @@ import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.editor.ScrollType
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleTypeId
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.modifyModules
 import com.intellij.openapi.roots.ModuleRootModificationUtil
+import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.ui.playback.commands.ActionCommand
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.psi.JavaCodeFragment
 import com.intellij.psi.JavaCodeFragmentFactory
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
 import com.intellij.psi.xml.XmlFile
-import com.intellij.refactoring.suggested.startOffset
 import com.intellij.ui.EditorTextField
 import com.intellij.util.xml.DomManager
 import java.awt.event.ActionEvent
 import java.awt.event.InputEvent
 import java.awt.event.KeyEvent
+import java.net.URI
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.IdentityHashMap
 import javax.swing.AbstractAction
 import javax.swing.JComponent
@@ -46,39 +54,25 @@ class LocateToolWindow(private val project: Project, toolWindow: ToolWindow) {
     private lateinit var locatorFragment: JavaCodeFragment
     private lateinit var locatorField: EditorTextField
 
+    companion object {
+        const val moduleName = "UI Surveyor (Highlighting)"
+    }
+
     fun createUIComponents() {
-        val module = project.modifyModules {
-            val module = newNonPersistentModule(
-                "(Surveyor - UI Automator support)",
-                ModuleTypeId.JAVA_MODULE
-            )
-
-            module
-        }
-
-        ModuleRootModificationUtil.updateModel(module) { model ->
-            val library = model.moduleLibraryTable.createLibrary("uiautomator")
-            library.modifiableModel.apply {
-                addJarDirectory(
-                    "file://C:/Users/const/.gradle/caches/transforms-3/7d370af9595cf4acefb849f695fd5ecb/transformed",
-                    true
-                )
-
-                ApplicationManager.getApplication().invokeAndWait {
-                    WriteAction.run<RuntimeException> { commit() }
-                }
-            }
-        }
+        val module = ModuleManager.getInstance(project).findModuleByName(moduleName)
+            ?: createModuleForHighlighting()
 
         val modulePsi = JavaModuleGraphUtil.findDescriptorByModule(module, false)
 
-        val editorCode = JavaCodeFragmentFactory.getInstance(project).createCodeBlockCodeFragment(
+        val importedClasses = listOf(UiSelector::class.java, By::class.java)
+            .joinToString(",") { it.name }
+        val editorCode = JavaCodeFragmentFactory.getInstance(project).createExpressionCodeFragment(
             "new UiSelector()",
             modulePsi,
-            true).apply {
-            addImportsFromString(
-                "androidx.test.uiautomator.UiSelector,androidx.test.uiautomator.By"
-            )
+            null,
+            true
+        ).apply {
+            addImportsFromString(importedClasses)
         }
         val editorDocument = PsiDocumentManager.getInstance(project).getDocument(editorCode)!!
         val editorField = EditorTextField(editorDocument, project, JavaFileType.INSTANCE)
@@ -108,6 +102,44 @@ class LocateToolWindow(private val project: Project, toolWindow: ToolWindow) {
         locatorField = editorField
     }
 
+    private fun createModuleForHighlighting(): Module {
+        return project.modifyModules {
+            val modulePath = Paths.get(project.projectFilePath!!).parent.resolve("$moduleName.iml")
+            val module = newModule(
+                modulePath.toString(),
+                ModuleTypeId.JAVA_MODULE
+            )
+
+            val automatorClass = UiSelector::class.java
+            ModuleRootModificationUtil.updateModel(module) { model ->
+                val automatorJarFile = getAutomatorJarPath(automatorClass)
+                val library = model.moduleLibraryTable.createLibrary("uiautomator")
+                library.modifiableModel.apply {
+                    addRoot(
+                        VfsUtil.getUrlForLibraryRoot(automatorJarFile.toFile()),
+                        OrderRootType.CLASSES
+                    )
+
+                    ApplicationManager.getApplication().invokeAndWait {
+                        WriteAction.run<RuntimeException> { commit() }
+                    }
+                }
+            }
+            module
+        }
+    }
+
+    private fun getAutomatorJarPath(automatorClass: Class<UiSelector>): Path {
+        val classPath = automatorClass.name.replace('.', '/') + ".class"
+        val classUrl = automatorClass.classLoader.getResource(classPath)!!.toExternalForm()
+        val jarUrl = classUrl
+            .removeSuffix(classPath)
+            .removeSuffix("/")
+            .removeSuffix("!")
+            .replaceFirst(Regex("^.*(?=file:)"), "")
+        return Paths.get(URI(jarUrl))
+    }
+
     fun getContent(): JPanel = content
 
     fun getCurrentLocator(): String {
@@ -127,7 +159,7 @@ class LocateAction: AnAction() {
         val service = project?.getService(LocateToolHoldingService::class.java) ?: return
         val locator = service.getCurrentLocator() ?: return
 
-        val (editor, xmlFile) = FileEditorManager.getInstance(project).selectedEditorWithRemotes
+        val (editor, xmlFile) = FileEditorManager.getInstance(project).selectedEditors
             .asSequence()
             .mapNotNull { editor ->
                 val virtualFile = editor.file
@@ -157,7 +189,7 @@ class LocateAction: AnAction() {
             ?: return // TODO
 
         with((editor as TextEditor).editor) {
-            caretModel.moveToOffset(psiNode.startOffset, false)
+            caretModel.moveToOffset(psiNode.textOffset, false)
             scrollingModel.scrollToCaret(ScrollType.MAKE_VISIBLE)
         }
     }
