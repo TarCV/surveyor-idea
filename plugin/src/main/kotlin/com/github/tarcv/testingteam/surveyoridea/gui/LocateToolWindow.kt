a9cc11b3b7c58c79c20d391843b8c046ca3d464d
TarCV <tarcv@users.noreply.github.com>
2023-02-25 05:03:33 +0300

    Fix compiling forms

--- plugin/src/main/kotlin/com/github/tarcv/testingteam/surveyoridea/gui/LocateToolWindow.kt
+++ plugin/src/main/kotlin/com/github/tarcv/testingteam/surveyoridea/gui/LocateToolWindow.kt
@@ -19,21 +19,14 @@ package com.github.tarcv.testingteam.surveyoridea.gui
 
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiSelector
-import com.github.tarcv.testingteam.surveyoridea.filetypes.uix.Hierarchy
 import com.github.tarcv.testingteam.surveyoridea.services.LocateToolHoldingService
-import com.github.tarcv.testingteam.surveyor.Evaluator
-import com.github.tarcv.testingteam.surveyor.Node
-import com.github.tarcv.testingteam.surveyor.Properties
 import com.intellij.codeInsight.daemon.impl.analysis.JavaModuleGraphUtil
 import com.intellij.ide.highlighter.JavaFileType
-import com.intellij.lang.xml.XMLLanguage
-import com.intellij.notification.NotificationType
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.editor.ScrollType
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleTypeId
@@ -47,17 +40,13 @@ import com.intellij.openapi.wm.ToolWindow
 import com.intellij.psi.JavaCodeFragment
 import com.intellij.psi.JavaCodeFragmentFactory
 import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiManager
-import com.intellij.psi.xml.XmlFile
 import com.intellij.ui.EditorTextField
-import com.intellij.util.xml.DomManager
 import java.awt.event.ActionEvent
 import java.awt.event.InputEvent
 import java.awt.event.KeyEvent
 import java.net.URI
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.IdentityHashMap
 import javax.swing.AbstractAction
 import javax.swing.JComponent
 import javax.swing.JPanel
@@ -67,7 +56,7 @@ import javax.swing.KeyStroke
 class LocateToolWindow(private val project: Project, toolWindow: ToolWindow) {
     private lateinit var content: JPanel
     private lateinit var locatorFragment: JavaCodeFragment
-    private lateinit var locatorField: EditorTextField
+    private lateinit var locatorField: JPanel
     private lateinit var toolbar: JComponent
 
     companion object {
@@ -170,98 +159,5 @@ class LocateToolWindow(private val project: Project, toolWindow: ToolWindow) {
         return locatorFragment.importsToString()
             .split(Regex("""\s*,\s*"""))
             .joinToString("", postfix = locatorFragment.text) { "import $it; " }
-    }
-}
-class LocateAction: AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-        val project = getEventProject(e)
-        val service = project?.getService(LocateToolHoldingService::class.java) ?: return
-        val locator = service.getCurrentLocator() ?: return
-
-        val (editor, xmlFile) = FileEditorManager.getInstance(project).selectedEditors
-            .asSequence()
-            .mapNotNull { editor ->
-                val virtualFile = editor.file
-                val psiFile = virtualFile?.let { it1 -> PsiManager.getInstance(project).findFile(it1) }
-                val xmlFile = psiFile?.viewProvider?.getPsi(XMLLanguage.INSTANCE) as? XmlFile
-                xmlFile?.let {
-                    editor to it
-                }
-            }
-            .firstOrNull()
-            ?: return project.notify(
-                "Can't locate an element when no UI Automator dump is focussed",
-                NotificationType.ERROR
-            )
-
-        val uix = DomManager.getDomManager(project).getFileElement(xmlFile, Hierarchy::class.java)
-            ?: return project.notify(
-                "Can't locate an element when no UI Automator dump is focussed",
-                NotificationType.ERROR
-            )
-
-        val mapping = IdentityHashMap<Node, com.github.tarcv.testingteam.surveyoridea.filetypes.uix.Node>()
-        val nodes = uix.rootElement.nodes
-            .map { convert(it, mapping) }
-
-        val rootNode = when {
-            nodes.size > 1 -> Node(null, emptyMap(), nodes, true)
-            nodes.size == 1 -> nodes[0]
-            else -> return project.notify(
-                "Can't locate an element when the focussed dump has multiple root nodes",
-                NotificationType.ERROR
-            )
-        }
-
-        val psiNode = Evaluator()
-            .evaluate(rootNode, locator)
-            .let { mapping[it] }
-            ?.xmlElement
-            ?: return project.notify(
-                "No elements were found",
-                NotificationType.WARNING
-            )
-
-        project.notify("Found an element", NotificationType.INFORMATION)
-        with((editor as TextEditor).editor) {
-            caretModel.moveToOffset(psiNode.textOffset, false)
-            scrollingModel.scrollToCaret(ScrollType.MAKE_VISIBLE)
-        }
-    }
-
-    private fun convert(
-        node: com.github.tarcv.testingteam.surveyoridea.filetypes.uix.Node,
-        mapping: MutableMap<Node, com.github.tarcv.testingteam.surveyoridea.filetypes.uix.Node>
-    ): Node {
-        val props: Map<Properties<*>, Any?> = listOf(
-            Properties.IS_CHECKABLE to node.checkable.value,
-            Properties.IS_CHECKED to node.checked.value,
-            Properties.CLASS_NAME to node.`clazz`.value,
-            Properties.IS_CLICKABLE to node.clickable.value,
-            Properties.ACCESSIBILITY_DESCRIPTION to node.contentDesc.value,
-            Properties.IS_ENABLED to node.enabled.value,
-            Properties.IS_FOCUSABLE to node.focusable.value,
-            Properties.IS_FOCUSED to node.focused.value,
-            Properties.IS_LONG_CLICKABLE to node.longClickable.value,
-            Properties.PACKAGE_NAME to node.`package`.value,
-            Properties.IS_PASSWORD_FIELD to node.password.value,
-            Properties.RESOURCE_ID to node.resourceId.value,
-            Properties.IS_SCROLLABLE to node.scrollable.value,
-            Properties.IS_SELECTED to node.selected.value,
-            Properties.TEXT to node.text.value,
-        ).filter { it.second != null }.toMap()
-
-        val out = Node(
-            null,
-            props,
-            node.nodes.map { convert(it, mapping) },
-            true // TODO
-        ).apply {
-            finalizeChildren()
-        }
-
-        mapping[out] = node
-
-        return out
     }
 }
