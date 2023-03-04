2e9fb776019836936981eaa605b19579994f3c2c
TarCV <tarcv@users.noreply.github.com>
2023-03-04 03:56:18 +0300

    Fix logging

--- droid-stubs/src/main/kotlin/android/util/Log.kt
+++ droid-stubs/src/main/kotlin/android/util/Log.kt
@@ -17,24 +17,22 @@
  */
 package android.util
 
-import org.slf4j.LoggerFactory
+import com.github.tarcv.testingteam.surveyor.Logger
 
 class Log {
     companion object {
-        private val logger = LoggerFactory.getLogger(Log::class.java)
-
         @JvmStatic
         fun isLoggable(a: String, b: Int): Boolean = true
 
         @JvmStatic
         fun d(tag: String, message: String): Int {
-            logger.debug("$tag $message")
+            Logger.debug("$tag $message")
             return 0
         }
 
         @JvmStatic
         fun i(tag: String, message: String): Int {
-            logger.info("$tag $message")
+            Logger.info("$tag $message")
             return 0
         }
     }
