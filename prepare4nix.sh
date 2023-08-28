cb3293d6db0691aaccb5d721487c34e051f2039b
TarCV <tarcv@users.noreply.github.com>
2023-08-29 00:51:26 +0300

    Update build

--- prepare4nix.sh
+++ prepare4nix.sh
@@ -10,8 +10,8 @@ sort -u -o artifacts.lst artifacts.lst
   # These dependencies are not found by the above commands:
   echo 'https://plugins.gradle.org/m2/org/jetbrains/kotlin/kotlin-reflect/1.6.10/kotlin-reflect-1.6.10.jar'
   echo 'https://plugins.gradle.org/m2/org/jetbrains/kotlin/kotlin-reflect/1.6.10/kotlin-reflect-1.6.10.pom'
-  echo 'https://plugins.gradle.org/m2/org/jetbrains/kotlin/kotlin-script-runtime/1.8.20/kotlin-script-runtime-1.8.20.jar'
-  echo 'https://plugins.gradle.org/m2/org/jetbrains/kotlin/kotlin-script-runtime/1.8.20/kotlin-script-runtime-1.8.20.pom'
+  echo 'https://plugins.gradle.org/m2/org/jetbrains/kotlin/kotlin-script-runtime/1.9.10/kotlin-script-runtime-1.9.10.jar'
+  echo 'https://plugins.gradle.org/m2/org/jetbrains/kotlin/kotlin-script-runtime/1.9.10/kotlin-script-runtime-1.9.10.pom'
 } >> artifacts.lst
 # TODO: Remove artifacts that are missing in $HOME/.gradle
 # TODO: Compare actual hashes of files in $HOME/.gradle with hashes in verification-metadata.xml
