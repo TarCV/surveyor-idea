f90b2c42e735f289aabc48e927ca6a58add4f7ce
TarCV <tarcv@users.noreply.github.com>
2024-01-02 01:24:46 +0300

    Update IDE versions for tests

--- test-noj.sh
+++ test-noj.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-IDE_CODE=PC IDE_VERSION=2022.3.3 PLUGIN_PATH="$(ls "$(pwd)"/plugin/build/distributions/*.zip)" ./gradlew :plugin-test:test "$@"
+IDE_CODE=PC IDE_VERSION=2023.3.2 PLUGIN_PATH="$(ls "$(pwd)"/plugin/build/distributions/*.zip)" ./gradlew :plugin-test:test "$@"
